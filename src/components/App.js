9fa4904bba380762369cd10707fad0f0bdea0b01
Anuj Kumar <101552403+wtricks@users.noreply.github.com>
2023-10-31 20:57:28 +0530

    gh

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Counter from './Counter'
 
 const App = () => {
   return (
     <div id="main">
         {/* Do not remove the main div */}
+        <Counter />
     </div>
   )
 }
