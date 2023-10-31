174a6751a4e64457a476e5aa1782a8e673c121ef
Anuj Kumar <101552403+wtricks@users.noreply.github.com>
2023-10-31 21:01:28 +0530

    hj

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,10 +4,10 @@ export default function Counter() {
     const [map, setMap] = React.useState([1, 2, 3, 4, 5])
 
     return (
-        <ol>
+        <ol id="relativeList">
             {
                 map.map((item, index) => (
-                    <li key={index}>relativeListItem{index}</li>
+                    <li id={'relativeListItem' + (index+1)} key={index}>relativeListItem{index}</li>
                 ))
             }
         </ol>
